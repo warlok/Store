9924d462023f345fa904c26c14729cb382e12847
warlok <Dean_2004>
2015-04-22 11:59:23 +0300

    Завершонный магазин по первым 10-ти урокам

---
+++ src/com/kademika/Store/Market/Objects/Type.java
@@ -0,0 +1,5 @@
+package com.kademika.Store.Market.Objects;
+
+public enum Type {
+	Mammal, Bird, Reptile, Fish, Amphibian, Arthropods;
+}
---
+++ src/com/kademika/Store/MarketDemo.java
@@ -0,0 +1,16 @@
+package com.kademika.Store;
+
+import com.kademika.Store.Market.Market;
+import com.kademika.Store.Market.MarketUI;
+
+public class MarketDemo {
+
+    public static void main(String[] args) {
+
+        Market m = new Market();
+        m.init();
+        new MarketUI(m);
+
+    }
+
+}
